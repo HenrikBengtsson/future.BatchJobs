cc202ac06c0bf475305d9730e71e7df80517a0ea
hb <hb@aroma-project.org>
2015-02-08 17:01:07 -0800

    Handling globals

--- R/async.R
+++ R/async.R
@@ -25,48 +25,13 @@ async <- function(expr, envir=parent.frame()) {
 
   if (debug) { mcat("Expression:\n"); mprint(expr) }
 
-  ## Function to be run by the parallel evaluator
-  fun <- eval(substitute(function(...) {
-    R.utils::withSink(str(list(...)), file="T:/foo.txt")
-    .expr.
-  }, list(.expr.=expr)))
-  if (debug) str(list(expr=expr, fun=fun))
-
-
-  ## Identify globals of fun(), i.e. objects that
-  ## fun() needs but are not assigned locally.  They
-  ## may be availble globally
-  globalNames <- codetools::findGlobals(fun)
-  if (debug) str(list(expr=expr, fun=fun, globalsNames=globalNames))
-
-  ## Get all globals
-  globals <- lapply(globalNames, FUN=get, envir=envir, inherits=TRUE)
-  names(globals) <- globalNames
-  if (debug) str(list(expr=expr, fun=fun, globals=globals))
-
-  if (debug) {
-    ## DEBUG: Check if the function can be evaluated
-#    ttt <- fun()
-#    mcat("Function fun() evaluated.\n")
-#    mprint(ttt)
-  }
-
   ## Create temporary registry
   reg <- tempRegistry()
   if (debug) mprint(reg)
 
-  ## AD HOC: submitJobs() tried to locate all variables
-  ## when saving the function 'fun'.  Here it will also
-  ## try to save the variable with name according to 'name',
-  ## but won't find one.  So, we create a dummy one here.
-#  rm(list=c("expr"), inherits=FALSE)
-
-  batchExport(reg, ll=globals)
-
   ## Create job
-  id <- batchMap(reg, fun=fun, 0L, more.args=list(a=1))
+  id <- batchEval(reg, exprs=list(expr), globals=TRUE, envir=envir)
   if (debug) mprintf("Created job #%d\n", id)
-
 
   ## Submit job
   submitJobs(reg, ids=id)
---
+++ tests/async-parallel.R
@@ -0,0 +1,40 @@
+R.utils::use()
+use("async")
+
+oopts <- options(warn=1, "async::debug"=TRUE)
+
+## Test that async() works when there are no globals
+t1 <- async({ x <- 1 })
+print(t1)
+
+## Test that async() works when there are globals
+a <- 2
+t2 <- async({ x <- a })
+print(t2)
+
+## Test that async() works when there are errors
+t3 <- async({ x <- 3; stop("Woops!"); x })
+print(t3)
+
+
+## Validate results
+v1 <- await(t1)
+print(v1)
+stopifnot(v1 == 1)
+v2 <- await(t2)
+print(v2)
+stopifnot(v2 == a)
+res <- tryCatch({
+  v3 <- await(t3)
+  print(v3)
+  v3
+}, error = function(ex) {
+  cat(ex$message)
+  cat("\nTEST OK!\n")
+  NULL
+})
+stopifnot(is.null(res))
+
+
+## Cleanup
+options(oopts)
