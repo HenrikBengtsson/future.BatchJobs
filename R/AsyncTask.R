f1e862170edd2fed49228cb08a189d856c1e0099
hb <hb@aroma-project.org>
2015-02-08 23:27:26 -0800

    Now the delay interval for result pooling is growing geometrically

--- R/AsyncTask.R
+++ R/AsyncTask.R
@@ -168,7 +168,9 @@ await <- function(...) UseMethod("await")
 #' @param cleanup If TRUE, the registry is completely removed upon
 #' success, otherwise not.
 #' @param maxTries The number of polls before giving up.
-#' @param interval The number of seconds to wait between polls.
+#' @param delta The number of seconds to wait the first time.
+#' @param alpha A factor to scale up the waiting time in each iteration
+#' such that the waiting time in the k:th iteration is \code{alpha^k*delta}.
 #' @param ... Not used.
 #'
 #' @return The value of the evaluated expression.
@@ -178,9 +180,11 @@ await <- function(...) UseMethod("await")
 #' @importFrom R.methodsS3 throw
 #' @importFrom R.utils mprint mprintf mstr
 #' @importFrom BatchJobs getStatus getErrorMessages loadResult removeRegistry
-await.AsyncTask <- function(obj, cleanup=TRUE, maxTries=getOption("async::maxTries", 10L), interval=getOption("async::interval", 1.0), ...) {
+await.AsyncTask <- function(obj, cleanup=TRUE, maxTries=getOption("async::maxTries", Sys.getenv("R_ASYNC_MAXTRIES", 1000)), delta=getOption("async::interval", 1.0), alpha=1.01, ...) {
   throw <- R.methodsS3::throw
 
+  maxTries <- as.integer(maxTries)
+  
   debug <- getOption("async::debug", FALSE)
   if (debug) mprintf("Polling...")
 
@@ -192,6 +196,7 @@ await.AsyncTask <- function(obj, cleanup=TRUE, maxTries=getOption("async::maxTri
   finished <- FALSE
   stat <- NULL
   tries <- 1L
+  interval <- delta
   while (tries <= maxTries) {
     stat <- status(obj)
     if (any(c("done", "error", "expired") %in% stat)) {
@@ -200,6 +205,7 @@ await.AsyncTask <- function(obj, cleanup=TRUE, maxTries=getOption("async::maxTri
     }
     if (debug) { mprintf("\n"); mprint(stat) }
     Sys.sleep(interval)
+    interval <- alpha*interval
     tries <- tries + 1L
   }
 
@@ -217,11 +223,21 @@ await.AsyncTask <- function(obj, cleanup=TRUE, maxTries=getOption("async::maxTri
       throw("BatchJobExpiration: ", msg)
     }
   } else {
-    throw(sprintf("AsyncNotReadyError: Polled for results %d times every %g seconds, but asynchroneous evaluation is still running: %s", tries, interval, reg))
+    throw(sprintf("AsyncNotReadyError: Polled for results %d times every %g seconds, but asynchroneous evaluation is still running: BatchJobs registry '%s' (%s)", tries, interval, reg$id, reg$file.dir))
   }
 
   ## Cleanup?
-  if (cleanup) removeRegistry(reg, ask = "no")
+  if (cleanup) {
+    interval <- 0.1*delta
+    for (kk in 1:10) {
+      if (isTRUE(try(removeRegistry(reg, ask="no"), silent=TRUE))) break
+      Sys.sleep(interval)
+      interval <- alpha*interval
+    }
+    if (file_test("-d", reg$file.dir)) {
+      warning(sprintf("Failed to remove BatchJobs registry '%s' (%s)", reg$id, reg$file.dir))
+    }
+  }
 
   res
 } # await()
