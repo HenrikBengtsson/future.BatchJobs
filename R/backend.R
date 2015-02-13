55d6de10f2e1e27717acd38266c7114837058382
hb <hb@aroma-project.org>
2015-02-12 23:11:24 -0800

    backend() no longer warns

--- R/backend.R
+++ R/backend.R
@@ -111,7 +111,7 @@ backend <- local({
     what <- what[1L]
 
     ## Inform about dropped requests?
-    if (length(dropped) > 0L && explicit_what) {
+    if (length(dropped) > 0L && explicit_what && getOption("async::on_unkown_backend", "ignore") == "warn") {
       warning(sprintf("Some of the preferred backends (%s) are either not available or not supported on your operating system ('%s'). Will use the following backend: %s", paste(sQuote(dropped), collapse=", "), .Platform$OS, sQuote(what)))
     }
 
