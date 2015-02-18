5ca8bfdf4cc3465cb6ea50c3d27df9dced615465
hb <hb@aroma-project.org>
2015-02-17 16:41:28 -0800

    Added example(asyncEvalQ) and documented substitute(..., env=list())

---
+++ inst/asyncEvalQ.R
@@ -0,0 +1,28 @@
+backend(c("multicore", "local"))
+
+## Setup three expressions
+exprs <- list(
+  A = substitute({ x <- 0.1 }, env=list()),
+  B = substitute({ y <- 0.2 }, env=list()),
+  C = substitute({ z <- a+0.3 }, env=list())
+)
+str(exprs)
+
+## Evaluate expressions asynchronously
+
+# Define global 'a' used in expression 'C'
+a <- 1
+
+lenv <- asyncEvalQ(exprs=exprs)
+message("Number of async evaluations: ", length(env))
+message("Async evaluations names: ", paste(sQuote(names(env)), collapse=", "))
+
+message("Values/Results:")
+
+## Sleep until value for expression 'B' is available
+value <- lenv$B
+message("Value 'B': ", value)
+
+## Sleep until all results are available
+values <- as.list(lenv)
+str(values)
