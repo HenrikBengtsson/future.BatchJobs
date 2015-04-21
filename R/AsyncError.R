1a228174462088439b0399bc305f86e4de19e4ef
hb <hb@aroma-project.org>
2015-04-21 11:55:12 -0700

    Forgot to add file

---
+++ R/AsyncError.R
@@ -0,0 +1,38 @@
+#' Exception class for async-related errors
+#'
+#' @export
+#' @importFrom R.oo setConstructorS3
+#' @importFrom R.oo extend
+#' @importFrom R.oo Exception
+AsyncError <- function(...) NULL  ## To please roxygen2
+setConstructorS3("AsyncError", function(...) {
+  extend(Exception(...), "AsyncError")
+})
+
+#' @export
+#' @importFrom R.oo setConstructorS3
+#' @importFrom R.oo extend
+#' @importFrom R.oo Exception
+AsyncTaskError <- function(...) NULL  ## To please roxygen2
+setConstructorS3("AsyncTaskError", function(..., task=NULL) {
+  error <- extend(AsyncError(...), "AsyncTaskError")
+  error$task <- task
+  error
+})
+
+#' @export
+#' @importFrom R.oo getMessage
+#' @importFrom R.oo trim
+getMessage.AsyncTaskError <- function(x, ...) {
+  msg <- NextMethod("getMessage")
+  task <- x$task
+  if (!is.null(task)) {
+    info <- capture.output(print(task))
+    info <- trim(info)
+    info <- paste(info, collapse="; ")
+    msg <- sprintf("%s [DEBUG INFORMATION: %s]", msg, info)
+  }
+  msg
+}
+
+
