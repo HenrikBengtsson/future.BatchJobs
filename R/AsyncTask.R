efe9f416dd274cd0bf66313ac018b039a826cdf7
hb <hb@aroma-project.org>
2015-04-21 17:44:48 -0700

    Now inspect() also accepts more complex input such as inspect(a$x), inspect(a[["x"]]) and inspect(a[[1]]).  It also accepts a character name such as inspect("x", envir=a).

--- R/AsyncTask.R
+++ R/AsyncTask.R
@@ -397,7 +397,71 @@ delete <- function(...) UseMethod("delete")
 #'
 #' @export
 inspect <- function(var, envir=parent.frame(), inherits=TRUE) {
-  name <- sprintf(".task_%s", deparse(substitute(var)))
+  expr <- substitute(var)
+  if (is.language(expr)) {
+    n <- length(expr)
+    name <- paste(deparse(expr), collapse="")
+    if (n != 1L && n != 3L) {
+      stop("Not a valid variable format: ", name, call.=FALSE)
+    }
+    if (n == 1L) {
+      ## Assignment to a variable name
+      if (!grepl("^[.a-zA-Z]", name)) {
+        stop("Not a valid variable name: ", name, call.=FALSE)
+      }
+    } else if (n == 3L) {
+      ## Assignment to enviroment via $ and [[
+      op <- expr[[1]]
+      if (op == "$" || op == "[[") {
+        ## Target
+        objname <- deparse(expr[[2]])
+        if (!exists(objname, envir=envir, inherits=TRUE)) {
+          stop(sprintf("Object %s not found: %s", sQuote(objname), name), call.=FALSE)
+        }
+        obj <- get(objname, envir=envir, inherits=TRUE)
+
+        ## Subset
+        idx <- expr[[3]]
+        if (is.symbol(idx)) {
+          idx <- deparse(idx)
+          if (op == "[[") {
+            if (!exists(idx, envir=envir, inherits=TRUE)) {
+              stop(sprintf("Object %s not found: %s", sQuote(idx), name), call.=FALSE)
+            }
+            idx <- get(idx, envir=envir, inherits=TRUE)
+          }
+        } else if (is.language(idx)) {
+          idx <- eval(idx, envir=envir)
+        }
+
+        ## Validate subetting, i.e. the 'idx'
+        if (length(idx) > 1L) {
+          stop(sprintf("Inspection can only be done on a single element at the time, not %d: %s", length(idx), name), call.=FALSE)
+        }
+
+        ## Special: listenv:s
+        if (inherits(obj, "listenv")) {
+          ## Get variable name to use
+          idx <- get_variable(obj, idx)
+        }
+
+        if (is.character(idx)) {
+        } else if (is.numeric(idx)) {
+          stop(sprintf("Inspection with indexed subsetting can not be done on a %s: %s", sQuote(mode(obj)), name), call.=FALSE)
+        } else {
+          stop(sprintf("Invalid subset %s: %s", sQuote(deparse(idx)), name), call.=FALSE)
+        }
+
+        if (is.environment(obj)) {
+          name <- idx
+          envir <- obj
+        } else {
+          stop(sprintf("Inspection can not be done to a %s; only to a variable or an environment: %s", sQuote(mode(obj)), name), call.=FALSE)
+        }
+      }
+    } # if (n == 3)
+  }
+
+  name <- sprintf(".task_%s", name)
   get(name, mode="list", envir=envir, inherits=inherits)
 }
-
