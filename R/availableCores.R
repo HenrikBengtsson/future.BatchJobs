9ccc9f86ce6b4e77c2e31e499a78073d28975758
hb <hb@aroma-project.org>
2015-05-20 15:44:16 -0700

    availableCores() can now be controlled by an option

--- R/availableCores.R
+++ R/availableCores.R
@@ -1,12 +1,12 @@
 #' Get number of available cores on current machine
 #'
-#' @param sources A character vector specifying which sources to use
-#'    to infer the number of available cores (and in what order).
+#' @param methods A character vector specifying how to infer the number
+#' of available cores (and in what order).
 #'
 #' @return Return a positive integer equal or greater to one.
 #'
 #' @details
-#' The following sources for inferring the number of cores are supported:
+#' The following methods for inferring the number of cores are supported:
 #' \itemize{
 #'  \item \code{"PBS"} -
 #'    Query Torque/PBS environment variable \code{PBS_NUM_PPN}.
@@ -22,14 +22,14 @@
 #'
 #' @export
 #' @importFrom parallel detectCores
-availableCores <- function(sources=c("PBS", "system")) {
-  for (source in sources) {
+availableCores <- function(methods=getOption("availableCoresMethods", c("PBS", "system"))) {
+  for (method in methods) {
     ncores <- NA_integer_
-    if (source == "PBS") {
+    if (method == "PBS") {
       ## Number of cores assigned by Torque/PBS?
       ncores <- as.integer(Sys.getenv("PBS_NUM_PPN", NA))
       if (is.finite(ncores) && ncores > 0L) break
-    } else if (source == "system") {
+    } else if (method == "system") {
       ## Number of cores available according to parallel::detectCores()
       ncores <- detectCores()
       if (is.finite(ncores) && ncores > 0L) break
