bf7f659cb17fdb409d1d601dde65e375c1ff0d86
hb <hb@aroma-project.org>
2015-06-03 15:55:54 -0700

    Added lazy future/lazy globals to tests

--- tests/lazyfuture.R
+++ tests/lazyfuture.R
@@ -9,3 +9,23 @@ print(isResolved(f))
 y <- value(f)
 print(y)
 stopifnot(y == 42L)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Lazy futures uses "lazy" globals
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## A global variable
+a <- 0
+f <- lazyfuture({
+  b <- 3
+  c <- 2
+  a * b * c
+})
+
+## Since 'a' is a global variable in _lazy_ future 'f',
+## which still hasn't been resolved, any changes to
+## 'a' until 'f' is resolved, will affect its value.
+a <- 7
+v <- value(f)
+print(v)
+stopifnot(v == 42)
