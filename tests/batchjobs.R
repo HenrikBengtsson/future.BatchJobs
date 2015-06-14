4a5f080356b0e944b4ed57f1e76d95cf3364da9b
hb <hb@aroma-project.org>
2015-06-14 11:54:42 -0700

    Add batchjobs() allowing for plan(batchjobs, backend=multicore) [#30]

---
+++ tests/batchjobs.R
@@ -0,0 +1,83 @@
+library("async")
+library("R.utils")
+
+ovars <- ls(envir=globalenv())
+oopts <- options(warn=1, "async::debug"=TRUE)
+
+message("*** batchjobs() ...")
+
+## An asynchroneous future without globals
+f <- batchjobs({ x <- 1 })
+print(f)
+v <- value(f)
+print(v)
+stopifnot(v == 1)
+
+
+## An asynchroneous future with globals
+a <- 2
+f <- batchjobs({ x <- a })
+print(f)
+v <- value(f)
+print(v)
+stopifnot(v == a)
+
+
+## An asynchroneous future with errors
+f <- batchjobs({ x <- 3; stop("Woops!"); x })
+print(f)
+v <- value(f, onError="return")
+print(v)
+
+res <- tryCatch({
+  v <- value(f)
+  print(v)
+  v
+}, error = function(ex) {
+  cat(ex$message)
+  NULL
+})
+stopifnot(is.null(res))
+
+
+message("*** future() w/ plan(batchjobs) ... OK")
+plan(batchjobs, backend=c("multicore=2", "local"))
+
+message("- future() / value() ...")
+f <- future({
+  42L
+})
+print(resolved(f))
+v <- value(f)
+print(v)
+stopifnot(v == 42L)
+
+message("- future assignment without globals ...")
+v %<=% {
+  42L
+}
+print(v)
+stopifnot(v == 42L)
+
+message("- future assignment with globals ...")
+a <- 3.14
+v %<=% { x <- a }
+print(v)
+stopifnot(v == a)
+
+message("- future assignment with error ...")
+v %<=% {
+  x <- 3
+  stop("Woops!")
+  x
+}
+res <- try(print(v), silent=TRUE)
+print(res)
+stopifnot(inherits(res, "try-error"))
+
+
+message("*** batchjobs() ... OK")
+
+## Cleanup
+options(oopts)
+rm(list=setdiff(ls(envir=globalenv()), ovars), envir=globalenv())
