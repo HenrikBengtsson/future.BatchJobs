9f0e318ef8543a4f299709f71ddc149dfccda0e2
hb <hb@aroma-project.org>
2015-10-14 13:16:09 -0700

    TESTS: Only 4 Mandelbrot planes per future for testing TESTS: The Mandelbrot demo requires R (>= 3.2.0)

--- tests/demo.R
+++ tests/demo.R
@@ -3,12 +3,18 @@ library(async)
 ovars <- ls()
 oopts <- options(warn=1L)
 
+options("R_FUTURE_DEMO_MANDELBROT_PLANES"=4L)
+
 message("*** Demos ...")
 
 message("*** Mandelbrot demo of the 'future' package ...")
 
-plan(batchjobs, batchjobs="local")
-demo("mandelbrot", package="future", ask=FALSE)
+if (getRversion() >= "3.2.0") {
+  plan(batchjobs, batchjobs="local")
+  demo("mandelbrot", package="future", ask=FALSE)
+} else {
+  message(" - This demo requires R (>= 3.2.0). Skipping test.")
+}
 
 message("*** Demos ... DONE")
 
