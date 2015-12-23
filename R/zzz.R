e66998138aee84377b3c79849d440691459f9638
hb <hb@aroma-project.org>
2015-12-23 08:02:21 -0800

    Tweak

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,15 @@
 ## covr: skip=all
 #' @importFrom R.utils removeDirectory
 .onUnload <- function(libpath) {
-  ## (a) Force finalize() on Future objects and their BatchJobs directories
+  ## (a) Force finalizer of Future objects to run such
+  ##     that their BatchJobs directories are removed
   gc()
+
   ## (b) Remove BatchJobs root directory unless non-empty
   path <- asyncPath()
   try(removeDirectory(path, recursive=FALSE, mustExist=FALSE), silent=TRUE)
 }
+
 
 .onLoad <- function(libname, pkgname) {
   backend("default")
