10a81ee6e9155c47dc9e95efe82399c33aec8f71
hb <hb@aroma-project.org>
2015-12-23 07:54:48 -0800

    CLEANUP: .onUnload() tries to clean up BatchJobs directories

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,13 @@
 ## covr: skip=all
+#' @importFrom R.utils removeDirectory
+.onUnload <- function(libpath) {
+  ## (a) Force finalize() on Future objects and their BatchJobs directories
+  gc()
+  ## (b) Remove BatchJobs root directory unless non-empty
+  path <- asyncPath()
+  try(removeDirectory(path, recursive=FALSE, mustExist=FALSE), silent=TRUE)
+}
+
 .onLoad <- function(libname, pkgname) {
   backend("default")
 }
