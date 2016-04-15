8acc950b6962c0c6029216cf527aa8cf9dab1313
hb <hb@aroma-project.org>
2016-04-15 09:11:33 -0700

    Generalize local function names

--- R/tempRegistry.R
+++ R/tempRegistry.R
@@ -1,27 +1,3 @@
-#' @importFrom R.utils isDirectory mkdirs captureOutput
-#' @importFrom utils sessionInfo
-asyncPath <- local({
-  path <- NULL
-  function(absolute=TRUE, create=TRUE) {
-    if (is.null(path)) {
-      id <- basename(tempdir())
-      id <- gsub("Rtmp", "", id, fixed=TRUE)
-      timestamp <- format(Sys.time(), format="%Y%m%d_%H%M%S")
-      dir <- sprintf("%s-%s", timestamp, id)
-      pathT <- file.path(".future", dir)
-      if (create && !isDirectory(pathT)) {
-        mkdirs(pathT)
-        pathnameT <- file.path(pathT, "sessioninfo.txt")
-        writeLines(captureOutput(print(sessionInfo())), con=pathnameT)
-      }
-      path <<- pathT
-    }
-    if (absolute) path <- file.path(getwd(), path)
-    path
-  }
-})
-
-
 #' @importFrom R.utils tempvar
 #' @importFrom BatchJobs makeRegistry
 tempRegistry <- local({
@@ -29,7 +5,7 @@ tempRegistry <- local({
 
   function(backend=NULL, prefix="BatchJobs_", path=NULL, file.dir=NULL, ...) {
     id <- tempvar(prefix=prefix, value=NA, envir=regs)
-    if (is.null(path)) path <- asyncPath()
+    if (is.null(path)) path <- futureCachePath()
     if (is.null(file.dir)) file.dir <- file.path(path, paste0(id, "-files"))
 
     ## Use a non-default backend?
