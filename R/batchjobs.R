a605de5cc54235c410ef09a3f8c61e65cb3ca074
hb <hb@aroma-project.org>
2016-05-20 16:23:00 -0700

    CLEANUP: Now batchjobs(..., backend) circumvent backend() [#61]

--- R/batchjobs.R
+++ R/batchjobs.R
@@ -15,6 +15,33 @@
 batchjobs <- function(expr, envir=parent.frame(), substitute=TRUE, backend=NULL, resources=list(), ...) {
   if (substitute) expr <- substitute(expr)
 
+  ## Backend explicitly specified?
+  if (!is.null(backend)) {
+    stopifnot(length(backend) == 1L, is.character(backend))
+
+    if (identical(backend, "local")) {
+      return(batchjobs_local(expr=expr, envir=envir, substitute=FALSE, ...))
+    } else if (identical(backend, "interactive")) {
+      return(batchjobs_interactive(expr=expr, envir=envir, substitute=FALSE, ...))
+    } else if (identical(backend, ".BatchJobs.R")) {
+      return(batchjobs_conf(expr=expr, envir=envir, substitute=FALSE, pathnames=NULL, ...))
+    } else if (grepl("^multicore", backend)) {
+        if (identical(backend, "multicore")) {
+        return(batchjobs_multicore(expr=expr, envir=envir, substitute=FALSE, ...))
+      } else if (grepl("^multicore=", backend)) {
+        workers <- as.integer(gsub("^multicore=", "", backend))
+        return(batchjobs_multicore(expr=expr, envir=envir, substitute=FALSE, workers=workers, ...))
+      } else if (grepl("^multicore-", backend)) {
+        total <- availableCores(constraint="multicore")
+        save <- as.integer(gsub("^multicore-", "", backend))
+        workers <- total - save
+        return(batchjobs_multicore(expr=expr, envir=envir, substitute=FALSE, workers=workers, ...))
+      }
+    } else if (file_test("-f", backend)) {
+      return(batchjobs_conf(expr=expr, envir=envir, substitute=FALSE, pathnames=backend, ...))
+    }
+  }
+
   ## 1. Create
   future <- BatchJobsFuture(expr=expr, envir=envir, substitute=FALSE,
                             backend=backend, resources=resources, ...)
