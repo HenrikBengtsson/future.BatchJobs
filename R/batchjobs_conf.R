e7930ecbfe0b6c6c4ef4d97b98186b1df049c5f1
hb <hb@aroma-project.org>
2016-05-20 17:27:43 -0700

    batchjobs_conf() gained argument 'conf' [#61]

--- R/batchjobs_conf.R
+++ R/batchjobs_conf.R
@@ -9,16 +9,20 @@
 #'              are search from.
 #' @param substitute Controls whether \code{expr} should be
 #'                   \code{substitute()}:d or not.
-#' @param pathnames Pathnames to one or more BatchJobs configuration
-#' files to be loaded in order.  If NULL, then the \pkg{BatchJobs}
-#' package will search for such configuration files.
+#' @param conf A BatchJobs configuration environment.
+#' @param pathnames (alternative) Pathnames to one or more BatchJobs
+#' configuration files to be loaded in order.  If NULL, then the
+#' \pkg{BatchJobs} package will search for such configuration files.
 #' @param \ldots Additional arguments passed to \code{\link{BatchJobsFuture}()}.
 #'
 #' @return An object of class \code{BatchJobsFuture}.
 #'
 #' @details
-#' If \code{pathnames=NULL} (default), the \pkg{BatchJobs} is configured
-#' to use (in order) all of following configuration files (if they exist):
+#' If \code{conf} is NULL (default), then the BatchJobs configuration will
+#' be created from a set of BatchJobs configuration files (R script files)
+#' as given by argument \code{pathnames}.  If none are specified (default),
+#' then \pkg{BatchJobs} is designed to use (in order) all of following
+#' configuration files (if they exist):
 #' \itemize{
 #'  \item \code{system("etc", "BatchJobs_global_config.R", package="BatchJobs")}
 #'  \item \code{~/.BatchJobs.R} (in user's home directory)
@@ -26,23 +30,27 @@
 #' }
 #'
 #' @export
-batchjobs_conf <- function(expr, envir=parent.frame(), substitute=TRUE, pathnames=NULL, ...) {
+batchjobs_conf <- function(expr, envir=parent.frame(), substitute=TRUE, conf=NULL, pathnames=NULL, ...) {
   findConfigs <- importBatchJobs("findConfigs")
   sourceConfFiles <- importBatchJobs("sourceConfFiles")
 
   if (substitute) expr <- substitute(expr)
 
-  if (is.null(pathnames)) {
-    ## This is now BatchJobs searches, cf. BatchJobs:::readConfs()
-    path <- find.package("BatchJobs")
-    pathnames  <- findConfigs(path)
-  }
+  if (is.null(conf)) {
+    if (is.null(pathnames)) {
+      ## This is now BatchJobs searches, cf. BatchJobs:::readConfs()
+      path <- find.package("BatchJobs")
+      pathnames  <- findConfigs(path)
+    }
 
-  stopifnot(length(pathnames) >= 1L, is.character(pathnames))
-  for (pathname in pathnames) {
-    if (!file_test("-f", pathname)) stop("File not found: ", sQuote(pathname))
+    stopifnot(length(pathnames) >= 1L, is.character(pathnames))
+    for (pathname in pathnames) {
+      if (!file_test("-f", pathname)) stop("File not found: ", sQuote(pathname))
+    }
+    conf <- sourceConfFiles(pathnames)
+  } else {
+    stopifnot(is.environment(conf))
   }
-  conf <- sourceConfFiles(pathnames)
 
   future <- BatchJobsFuture(expr=expr, envir=envir, substitute=FALSE,
                             conf=conf, ...)
