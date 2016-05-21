592167bae154bd925ffd52f9e5442e089b19040b
hb <hb@aroma-project.org>
2016-05-20 17:58:58 -0700

    Added batchjobs_lsf(), batchjobs_openlava(), batchjobs_sge(), batchjobs_slurm() and batchjobs_torque() futures [#61]

---
+++ R/batchjobs_torque.R
@@ -0,0 +1,91 @@
+#' BatchJobs LSF, OpenLava, SGE, Slurm and Torque futures
+#'
+#' LSF, OpenLava, SGE, Slurm and Torque BatchJobs futures are asynchronous
+#' and multiprocess, will not block, and will be evaluated on a compute
+#' cluster via a job scheduler.
+#'
+#' @param expr An R expression to be evaluated.
+#' @param envir The environment from which global environment
+#'              are search from.
+#' @param substitute Controls whether \code{expr} should be
+#'                   \code{substitute()}:d or not.
+#' @param pathname A BatchJobs template file (brew formatted).
+#' @param \ldots Additional arguments passed to \code{\link{BatchJobsFuture}()}.
+#'
+#' @return An object of class \code{BatchJobsFuture}.
+#'
+#' @aliases batchjobs_openlava batchjobs_sge batchjobs_slurm batchjobs_torque
+#' @export
+batchjobs_lsf <- function(expr, envir=parent.frame(), substitute=TRUE, pathname=NULL, ...) {
+  if (substitute) expr <- substitute(expr)
+
+  batchjobs_by_template(expr, envir=envir, substitute=FALSE, pathname=pathname, type="lsf", ...)
+}
+class(batchjobs_lsf) <- c("batchjobs_lsf", "batchjobs", "multiprocess", "future", "function")
+
+#' @export
+batchjobs_openlava <- function(expr, envir=parent.frame(), substitute=TRUE, pathname=NULL, ...) {
+  if (substitute) expr <- substitute(expr)
+
+  batchjobs_by_template(expr, envir=envir, substitute=FALSE, pathname=pathname, type="openlava", ...)
+}
+class(batchjobs_openlava) <- c("batchjobs_openlava", "batchjobs", "multiprocess", "future", "function")
+
+#' @export
+batchjobs_sge <- function(expr, envir=parent.frame(), substitute=TRUE, pathname=NULL, ...) {
+  if (substitute) expr <- substitute(expr)
+
+  batchjobs_by_template(expr, envir=envir, substitute=FALSE, pathname=pathname, type="sge", ...)
+}
+class(batchjobs_sge) <- c("batchjobs_sge", "batchjobs", "multiprocess", "future", "function")
+
+#' @export
+batchjobs_slurm <- function(expr, envir=parent.frame(), substitute=TRUE, pathname=NULL, ...) {
+  if (substitute) expr <- substitute(expr)
+
+  batchjobs_by_template(expr, envir=envir, substitute=FALSE, pathname=pathname, type="slurm", ...)
+}
+class(batchjobs_slurm) <- c("batchjobs_slurm", "batchjobs", "multiprocess", "future", "function")
+
+#' @export
+batchjobs_torque <- function(expr, envir=parent.frame(), substitute=TRUE, pathname=NULL, ...) {
+  if (substitute) expr <- substitute(expr)
+
+  batchjobs_by_template(expr, envir=envir, substitute=FALSE, pathname=pathname, type="torque", ...)
+}
+class(batchjobs_torque) <- c("batchjobs_torque", "batchjobs", "multiprocess", "future", "function")
+
+
+#' @importFrom BatchJobs makeClusterFunctionsLSF
+#' @importFrom BatchJobs makeClusterFunctionsOpenLava
+#' @importFrom BatchJobs makeClusterFunctionsSGE
+#' @importFrom BatchJobs makeClusterFunctionsSLURM
+#' @importFrom BatchJobs makeClusterFunctionsTorque
+batchjobs_by_template <- function(expr, envir=parent.frame(), substitute=TRUE, pathname=NULL, type=c("lsf", "openlava", "sge", "slurm", "torque"), ...) {
+  if (substitute) expr <- substitute(expr)
+  type <- match.arg(type)
+
+  makeCF <- switch(type,
+    lsf      = makeClusterFunctionsLSF,
+    openlava = makeClusterFunctionsOpenLava,
+    sge      = makeClusterFunctionsSGE,
+    slurm    = makeClusterFunctionsSLURM,
+    torque   = makeClusterFunctionsTorque
+  )
+
+  if (is.null(pathname)) {
+    path <- system.file("conf", package="future.BatchJobs")
+    filename <- sprintf("%s.brew", type)
+    pathname <- file.path("conf", filename)
+  }
+
+  conf <- makeCF(pathname)
+
+  future <- BatchJobsFuture(expr=expr, envir=envir, substitute=FALSE,
+                            conf=conf,
+                            backend=type, ...)
+
+  future <- run(future)
+
+  future
+} ## batchjobs_by_template()
