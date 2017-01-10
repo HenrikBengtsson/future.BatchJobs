62cc779375b1cc02015f15367ee1d6ae36a4b89b
hb <hb@aroma-project.org>
2017-01-10 10:29:25 -0800

    Add warnings about BatchJobs devel and old globals
    [ci skip]

--- R/zzz.R
+++ R/zzz.R
@@ -20,3 +20,12 @@
   }
 }
 
+#' @importFrom utils packageVersion
+.onLoad <- function(libpath, pkgname) {
+  ## NOTIFICATION: BatchJobs (> 1.6) and globals (<= 0.7.1)
+  if ((verB <- packageVersion("BatchJobs")) > "1.6" &&
+      packageVersion("globals") < "0.7.2") {
+    warning(sprintf("future.BatchJobs requires globals (>= 0.7.2) when using BatchJobs %s.  Please update the globals package.", verB))
+  }
+}
+
