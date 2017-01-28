2299612aec4ce2c0e170017e8a6564fa5f197419
hb <hb@aroma-project.org>
2017-01-27 20:48:59 -0800

    DEMO: Suggest batchjobs_local (not batchjobs_multicore which is not crossplatform and also unreliable)

--- demo/mandelbrot.R
+++ demo/mandelbrot.R
@@ -1,3 +1,3 @@
 library("future.BatchJobs")
 demo("mandelbrot", package="future", ask=FALSE)
-message("\n\nTIPS: Try this demo with for instance plan(batchjobs_multicore).\n")
+message("\n\nTIPS: Try this demo with for instance plan(batchjobs_local).\n")
