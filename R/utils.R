3da77820c4fe61a9c511fef4a7fe00d013de701b
Henrik Bengtsson <hb@aroma-project.org>
2019-06-07 23:39:31 -0700

    CLEANUP: Don't have R CMD check leave files behind [#77]

--- R/utils.R
+++ R/utils.R
@@ -175,3 +175,18 @@ result_has_errors <- function(result) {
   
   FALSE
 }
+
+import_from <- function(name, default = NULL, package) {
+  ns <- getNamespace(package)
+  if (exists(name, mode = "function", envir = ns, inherits = FALSE)) {
+    get(name, mode = "function", envir = ns, inherits = FALSE)
+  } else if (!is.null(default)) {
+    default
+  } else {
+    stop(sprintf("No such '%s' function: %s()", package, name))
+  }
+}
+
+import_future <- function(name, default = NULL) {
+  import_from(name, default = default, package = "future")
+}
